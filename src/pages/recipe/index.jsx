c39e7e62c652c29ee4b30ffac1a1087122fb131c
dee2020 <huzidi@gmail.com>
2022-02-12 19:22:49 +1100

    use card component instead of list

--- src/pages/recipe/index.jsx
+++ src/pages/recipe/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Collapse, Button, List } from 'antd';
+import { Collapse, Button, List, Card } from 'antd';
 import mockRecipe from '../../mock/recipe'
 
 const { Panel } = Collapse;
@@ -11,22 +11,42 @@ const RecipeList = props => {
   }
 
   return (
-    <Collapse bordered={false} defaultActiveKey={[1]}>
-      {mockRecipe.map((recipe) => (
-        <Panel header={recipe.recipeName} key={recipe.key}>
-          <List
-            size="small"
-            bordered
-            dataSource={recipe.ingredients}
-            renderItem={item =>
-              <List.Item>{item.ingredientName} {item.ingredientQuality}</List.Item>
-            }
-          />
-          <Button type="primary" onClick={() => console.log('cooking', recipe.key)}>Cook</Button>
-        </Panel>
-      ))
-      }
-    </Collapse >
+    <>
+      {/* <Collapse bordered={false} defaultActiveKey={[1]}>
+        {mockRecipe.map((recipe) => (
+          <Panel header={recipe.recipeName} key={recipe.key}>
+            <List
+              size="small"
+              bordered
+              dataSource={recipe.ingredients}
+              renderItem={item =>
+                <List.Item>{item.ingredientName} {item.ingredientQuality}</List.Item>
+              }
+            />
+            <Button type="primary" onClick={() => console.log('cooking', recipe.key)}>Cook</Button>
+          </Panel>
+        ))
+        }
+      </Collapse > */}
+      <List
+        grid={{ gutter: 16, lg: 3, md: 2, sm: 1 }}
+        dataSource={mockRecipe}
+        renderItem={item => (
+          <List.Item>
+            <Card title={item.recipeName}>
+              {item.ingredients.map((ingredients) => (
+                <>
+                  <p className='d-inline'>{ingredients.ingredientName}</p>
+                  <p className='d-inline'>{ingredients.ingredientQuality}</p>
+                  <br/>
+                </>
+              ))}
+              <Button type="primary" onClick={() => console.log('rowkey', item.key)}>Cook</Button>
+            </Card>
+          </List.Item>
+        )}
+      />
+    </>
   )
 }
 
